b5d7eb159cb84d00ed43845cb09eb7953b648548
Franco Trelles <franco.trellesm@gmail.com>
2021-01-22 16:33:58 -0500

    Simple component with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+};
 
 export default App;
